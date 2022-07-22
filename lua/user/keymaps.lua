06ad7fb8180931d91cd90f3cd0c6272c4d31ead3
Joel Singh <joelsingh788@gmail.com>
2022-07-22 03:15:42 -0400

    Add extra telescope functions
    They are find_files with home and root directory

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -57,6 +57,8 @@ keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
 -- Telescope
 keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+keymap("n", "<leader>fh", ":lua require('telescope.builtin').find_files({cwd = '~'})<CR>", opts)
+keymap("n", "<leader>fr", ":lua require('telescope.builtin').find_files({cwd = '/'})<CR>", opts)
 keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
 keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
 keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
