c80d574276cc99a48437f90f3fd81b4ba2bc3032
Joel Singh <joelsingh788@gmail.com>
2022-10-07 23:48:30 -0400

    Add line numbering options to options.lua

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,3 +1,5 @@
+vim.o.relativenumber = true
+vim.o.number = true
 vim.opt.backup = false                          -- creates a backup file
 vim.opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
 vim.opt.cmdheight = 1                           -- more space in the neovim command line for displaying messages
