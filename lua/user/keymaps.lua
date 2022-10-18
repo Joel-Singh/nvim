964c5c49a84edb7b05b6e74d8192d0ffee97dc99
Joel Singh <joelsingh788@gmail.com>
2022-10-18 15:05:22 -0400

    New tab kepmap

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -22,6 +22,7 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 keymap("n", "<C-c>", "<C-w>c", opts)
+keymap("n", "<leader>tn", ":tabnew %<CR>", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
