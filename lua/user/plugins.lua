d8727d19206e58e44098e8971cc5e902b7785cb1
Joel Singh <joelsingh788@gmail.com>
2022-10-23 10:08:33 -0400

    Format

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -103,7 +103,7 @@ return packer.startup(function(use)
   use { "ravenxrz/DAPInstall.nvim", commit = "8798b4c36d33723e7bba6ed6e2c202f84bb300de" }
 
   -- Personal
-  use {"mattn/emmet-vim"}
+  use { "mattn/emmet-vim" }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
