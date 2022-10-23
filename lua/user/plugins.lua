6f8df0516d6dd7ca6fdf39bc1025d9ab18d67d1b
Joel Singh <joelsingh788@gmail.com>
2022-10-23 10:08:44 -0400

    Add treesitter-context as a plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -104,6 +104,7 @@ return packer.startup(function(use)
 
   -- Personal
   use { "mattn/emmet-vim" }
+  use { "nvim-treesitter/nvim-treesitter-context" }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
