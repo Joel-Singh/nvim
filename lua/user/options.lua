e7f080f26404944fce84376d3f216e5293dadfe1
Joel Singh <joelsingh788@gmail.com>
2022-10-29 07:34:29 -0400

    Remove auto refresh on browser

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,9 +1,6 @@
 vim.o.relativenumber = true
 vim.o.number = true
 vim.api.nvim_command('autocmd BufWritePost *.bashrc :silent !source ~/.bashrc')
-vim.api.nvim_command('autocmd BufWritePost *.html :silent !xdotool search --class min windowactivate --sync \\%1 key F5 windowactivate $(xdotool getactivewindow)')
-vim.api.nvim_command('autocmd BufWritePost *.js :silent !xdotool search --class min windowactivate --sync \\%1 key F5 windowactivate $(xdotool getactivewindow)')
-vim.api.nvim_command('autocmd BufWritePost *.css :silent !xdotool search --class min windowactivate --sync \\%1 key F5 windowactivate $(xdotool getactivewindow)')
 vim.opt.backup = false                          -- creates a backup file
 vim.opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
 vim.opt.cmdheight = 1                           -- more space in the neovim command line for displaying messages
