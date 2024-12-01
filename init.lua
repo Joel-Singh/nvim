ef1716de2c455389916b5ce09a74765f6d7990f1
Joel Singh <joelsingh788@gmail.com>
2024-11-30 21:56:06 -0500

    Add keymap for lazygit

--- init.lua
+++ init.lua
@@ -99,6 +99,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('n', '<Leader>T', '<cmd>terminal<CR>ilazygit && e<CR>', { desc = 'Enter Lazygit' })
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
