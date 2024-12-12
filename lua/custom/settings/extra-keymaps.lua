6490713e450cc09d6cbd8d5a3a469536167e9dcb
Joel Singh <joelsingh788@gmail.com>
2024-12-12 02:34:26 -0500

    Enable tw on md files

--- lua/custom/settings/extra-keymaps.lua
+++ lua/custom/settings/extra-keymaps.lua
@@ -1,2 +1,4 @@
 vim.keymap.set('n', 'W', '<CMD>w<CR>', { desc = 'Write file' })
+
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, { pattern = { '*.md' }, command = 'set tw=60' })
 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, { pattern = { '*.md' }, command = 'Copilot disable' })
