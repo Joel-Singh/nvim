58a81e74ded3e7f6befb146a5c0c464e0d141db8
Joel Singh <joelsingh788@gmail.com>
2024-12-17 15:06:03 -0500

    Install treewalker

---
+++ lua/custom/plugins/treewalker.lua
@@ -0,0 +1,6 @@
+return {
+  'aaronik/treewalker.nvim',
+  opts = {
+    highlight = true, -- Whether to briefly highlight the node after jumping to it
+  },
+}
--- lua/custom/settings/extra-keymaps.lua
+++ lua/custom/settings/extra-keymaps.lua
@@ -2,3 +2,8 @@ vim.keymap.set('n', 'W', '<CMD>w<CR>', { desc = 'Write file' })
 
 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, { pattern = { '*.md' }, command = 'set tw=60' })
 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, { pattern = { '*.md' }, command = 'Copilot disable' })
+
+vim.api.nvim_set_keymap('n', '<C-j>', ':Treewalker Down<CR>', { noremap = true, desc = 'Treewalker Down' })
+vim.api.nvim_set_keymap('n', '<C-k>', ':Treewalker Up<CR>', { noremap = true, desc = 'Treewalker Up' })
+vim.api.nvim_set_keymap('n', '<C-h>', ':Treewalker Left<CR>', { noremap = true, desc = 'Treewalker Left' })
+vim.api.nvim_set_keymap('n', '<C-l>', ':Treewalker Right<CR>', { noremap = true, desc = 'Treewalker Right' })
