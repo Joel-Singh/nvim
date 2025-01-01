874569e16da4c1a68ab89c6f41cccafd03020ed8
Joel Singh <joelsingh788@gmail.com>
2025-01-01 17:49:39 -0500

    Add search symbols

--- lua/custom/settings/extra-keymaps.lua
+++ lua/custom/settings/extra-keymaps.lua
@@ -1,4 +1,5 @@
 vim.keymap.set('n', '<Leader>W', '<CMD>w<CR>', { desc = 'Write file' })
+vim.keymap.set('n', '<Leader>sy', '<CMD>Telescope symbols<CR>', { desc = '[S]earch S[y]mbols' })
 
 -- vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, { pattern = { '*.md' }, command = 'set tw=60' })
 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, { pattern = { '*.md' }, command = 'Copilot disable' })
