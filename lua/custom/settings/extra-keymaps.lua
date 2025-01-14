425fda5db3e5a95ec635edf289509df9fe441209
Joel Singh <joelsingh788@gmail.com>
2025-01-14 14:56:56 -0500

    Add keymap for ai chat

--- lua/custom/settings/extra-keymaps.lua
+++ lua/custom/settings/extra-keymaps.lua
@@ -1,5 +1,7 @@
 vim.keymap.set('n', '<Leader>W', '<CMD>w<CR>', { desc = 'Write file' })
 vim.keymap.set('n', '<Leader>sy', '<CMD>Telescope symbols<CR>', { desc = '[S]earch S[y]mbols' })
 
+vim.keymap.set('n', '<Leader>cc', '<CMD>CodeCompanionChat<CR>', { desc = '[C]ode [C]hat' })
+
 -- vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, { pattern = { '*.md' }, command = 'set tw=60' })
 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, { pattern = { '*.md' }, command = 'Copilot disable' })
