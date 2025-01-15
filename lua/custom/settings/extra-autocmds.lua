5b0a0e784478d298ae833d340d4fa1d82f36fb22
Joel Singh <joelsingh788@gmail.com>
2025-01-14 20:47:27 -0500

    Add auto cmd for latex

--- lua/custom/settings/extra-autocmds.lua
+++ lua/custom/settings/extra-autocmds.lua
@@ -1,3 +1,4 @@
 -- vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, { pattern = { '*.md' }, command = 'set tw=60' })
 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, { pattern = { '*.md' }, command = 'Copilot disable' })
 
+vim.api.nvim_create_autocmd({ 'BufWritePost' }, { pattern = { '*.tex' }, command = 'silent !pdflatex -output-directory=output % > /dev/null &' })
