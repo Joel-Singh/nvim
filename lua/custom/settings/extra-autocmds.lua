27b1bd93ff886f7689620d86001fed692c4f016c
Joel Singh <joelsingh788@gmail.com>
2025-01-17 12:18:04 -0500

    Disable copilot on tex

--- lua/custom/settings/extra-autocmds.lua
+++ lua/custom/settings/extra-autocmds.lua
@@ -1,5 +1,5 @@
 -- vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, { pattern = { '*.md' }, command = 'set tw=60' })
-vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, { pattern = { '*.md' }, command = 'Copilot disable' })
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, { pattern = { '*.md', '*.tex' }, command = 'Copilot disable' })
 
 vim.api.nvim_create_autocmd({ 'BufWritePost' }, { pattern = { '*.tex' }, command = 'silent !pdflatex -output-directory=output % > /dev/null &' })
 
