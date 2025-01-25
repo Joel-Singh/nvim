c298d3ba32c80a084e005c692cd9ba5041ecfd55
Joel Singh <joelsingh788@gmail.com>
2025-01-24 20:26:43 -0500

    Have hatched duck be a turtle

--- lua/custom/plugins/duck.lua
+++ lua/custom/plugins/duck.lua
@@ -1,7 +1,7 @@
 return {
   'tamton-aquib/duck.nvim',
   config = function()
-    vim.cmd 'command! DuckHatch :lua require("duck").hatch()'
+    vim.cmd 'command! DuckHatch :lua require("duck").hatch("🐢")'
     vim.cmd 'command! DuckCook :lua require("duck").cook()'
     vim.cmd 'command! DuckCookAll :lua require("duck").cook_all()'
   end,
