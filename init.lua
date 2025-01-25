0b14deddd6a52b5e0c4965c297ea2ac78a060a6a
Joel Singh <joelsingh788@gmail.com>
2025-01-24 20:27:54 -0500

    Add harper_ls

--- init.lua
+++ init.lua
@@ -608,6 +608,7 @@ require('lazy').setup({
         -- ts_ls = {},
         --
 
+        harper_ls = { filetypes = { 'markdown' } },
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
