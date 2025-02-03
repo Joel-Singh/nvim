ace3a5e4569fb7e084502b9dd9efa2c1dbeb9a1f
Joel Singh <joelsingh788@gmail.com>
2025-02-02 23:33:33 -0500

    Configure config for latex

--- lua/custom/settings/extra-autocmds.lua
+++ lua/custom/settings/extra-autocmds.lua
@@ -1,10 +1,18 @@
 -- vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, { pattern = { '*.md' }, command = 'set tw=60' })
 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, { pattern = { '*.md', '*.tex', '*.txt' }, command = 'Copilot disable' })
 
-vim.api.nvim_create_autocmd({ 'BufWritePost' }, { pattern = { '*.tex' }, command = 'silent !pdflatex -output-directory=output % > /dev/null &' })
+vim.api.nvim_create_autocmd(
+  { 'BufWritePost' },
+  { pattern = { '*.tex' }, command = 'silent !pdflatex -output-directory=%:h' .. '/output' .. ' % > /dev/null &' }
+)
 
 vim.api.nvim_create_autocmd('InsertLeave', {
   pattern = '*.md',
+  command = 'write | doautocmd BufWritePre | doautocmd BufWritePost',
+})
+
+vim.api.nvim_create_autocmd('InsertLeave', {
+  pattern = '*.tex',
   command = 'write | doautocmd BufWritePre | doautocmd BufWritePost',
 })
 
