cd75644e5ba587c763f8575e0cd7321f5b94e77d
Joel Singh <joelsingh788@gmail.com>
2025-02-22 22:33:51 -0500

    Modify follow paths

--- lua/custom/plugins/telekasten.lua
+++ lua/custom/plugins/telekasten.lua
@@ -2,9 +2,9 @@ return {
   'renerocksai/telekasten.nvim',
   dependencies = { 'nvim-telescope/telescope.nvim' },
   opts = {
-    home = vim.fn.expand '~/Personal/note-graphs/Guiding-Principles/home',
-    dailies = vim.fn.expand '~/Personal/note-graphs/Guiding-Principles/dailies',
-    weeklies = vim.fn.expand '~/Personal/note-graphs/Guiding-Principles/weeklies',
-    templates = vim.fn.expand '~/Personal/note-graphs/Guiding-Principles/weeklies',
+    home = vim.fn.expand '~/Personal/Guiding-Principles/home',
+    dailies = vim.fn.expand '~/Personal/Guiding-Principles/dailies',
+    weeklies = vim.fn.expand '~/Personal/Guiding-Principles/weeklies',
+    templates = vim.fn.expand '~/Personal/Guiding-Principles/weeklies',
   },
 }
