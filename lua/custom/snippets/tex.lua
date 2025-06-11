5bc5b5351f1ab15b8ed446f714dc4fa489cfdffc
Joel Singh <joelsingh788@gmail.com>
2025-06-11 11:50:37 -0400

    Format

--- lua/custom/snippets/tex.lua
+++ lua/custom/snippets/tex.lua
@@ -36,175 +36,171 @@ local function create_snippet_not_auto(trig, char, nodes)
 end
 
 return {
-  create_snippet_not_auto('emptyset', [[\emptyset]]),
-  create_snippet('geq', [[\geq]]),
-  create_snippet('leq', [[\leq]]),
-  create_snippet('cdot', [[\cdot]]),
-  create_snippet_not_auto('subset', [[\subseteq]]),
-  create_snippet('superset', [[\supseteq]]),
-  create_snippet('tdd', [[\text{ def} \\]]),
-  create_snippet('excsubset', [[\subset]]),
-  create_snippet('excsuperset', [[\supset]]),
-  create_snippet('-union', [[\cup]]),
-  create_snippet_not_auto('ellipses', [[\ldots]]),
-  create_snippet_not_auto('intersection', [[\cap]]),
-  create_snippet('nchoosek', [[\binom{<>}{<>}]], { i(1), i(2) }),
-  create_snippet('txx', [[\text{<>}]], { i(1) }),
-  create_snippet('dmm', "\\[<>\\]", { i(1) }),
-  s({ trig = 'mm', snippetType = 'autosnippet', wordTrig=true }, fmta([[$<>$]], { i(1) })),
-  create_snippet('-set', [[\{<>\}]], { i(1) }),
-  create_snippet_not_auto('powerset', [[\mathcal{P}(<>)]], { i(1) }),
-  create_snippet('x->', [[\xrightarrow{<>}]], { i(1) }),
-  create_snippet('->x', [[\overrightarrow{<>}]], { i(1) }),
-  create_snippet('sqrt', [[\sqrt{<>}]], { i(1) }),
-  create_snippet('zerovec', [[\mathbf{0}]]),
-  create_snippet('cvv', [[\langle <> \rangle]], { i(1) }),
-  create_snippet('ihat', [[\mathbf{\hat{i}}]]),
-  create_snippet('jhat', [[\mathbf{\hat{j}}]]),
-  create_snippet('khat', [[\mathbf{\hat{k}}]]),
-  create_snippet('nll', [[\newline]]),
-  create_snippet('npp', [[\newpage]]),
-  create_snippet('tcred', [[\textcolor{red}{<>}]], { i(1) }),
-  create_snippet('img', [[![<>](<>){width=<> height=<>}]], { i(1, 'alt-text'), i(2, 'link'), i(3), i(4) }),
-  s(
-    { trig = 'summation' },
-    fmta(
-      [[\sum_{<>}^{<>}<>]],
-      { i(1, 'from'), i(2, 'to'), i(3, 'summand')}
-    )
-  ),
-  s(
-    { trig = 'productation' },
-    fmta(
-      [[\prod_{<>}^{<>}<>]],
-      { i(1, 'from'), i(2, 'to'), i(3, 'productand')}
-    )
-  ),
-  create_snippet('-subsection', [[\subsection{<>}]], { i(1) }),
-  s({ trig = 'italic' }, fmta([[\textit{<>}]], { i(1) })),
-  s({ trig = 'bold' }, fmta([[\textbf{<>}]], { i(1) })),
-  s({ trig = 'mono' }, fmta([[\texttt{<>}]], { i(1) })),
-  s({ trig = 'mnn', snippetType='autosnippet', wordTrig=false }, fmta([[\texttt{<>}]], { i(1) })),
-  s(
-    { trig = 'itemize' },
-    fmta(
-      [[
+	create_snippet_not_auto('emptyset', [[\emptyset]]),
+	create_snippet('geq', [[\geq]]),
+	create_snippet('leq', [[\leq]]),
+	create_snippet('cdot', [[\cdot]]),
+	create_snippet_not_auto('subset', [[\subseteq]]),
+	create_snippet('superset', [[\supseteq]]),
+	create_snippet('tdd', [[\text{ def} \\]]),
+	create_snippet('excsubset', [[\subset]]),
+	create_snippet('excsuperset', [[\supset]]),
+	create_snippet('-union', [[\cup]]),
+	create_snippet_not_auto('ellipses', [[\ldots]]),
+	create_snippet_not_auto('intersection', [[\cap]]),
+	create_snippet('nchoosek', [[\binom{<>}{<>}]], { i(1), i(2) }),
+	create_snippet('txx', [[\text{<>}]], { i(1) }),
+	create_snippet('dmm', "\\[<>\\]", { i(1) }),
+	s({ trig = 'mm', snippetType = 'autosnippet', wordTrig = true }, fmta([[$<>$]], { i(1) })),
+	create_snippet('-set', [[\{<>\}]], { i(1) }),
+	create_snippet_not_auto('powerset', [[\mathcal{P}(<>)]], { i(1) }),
+	create_snippet('x->', [[\xrightarrow{<>}]], { i(1) }),
+	create_snippet('->x', [[\overrightarrow{<>}]], { i(1) }),
+	create_snippet('sqrt', [[\sqrt{<>}]], { i(1) }),
+	create_snippet('zerovec', [[\mathbf{0}]]),
+	create_snippet('cvv', [[\langle <> \rangle]], { i(1) }),
+	create_snippet('ihat', [[\mathbf{\hat{i}}]]),
+	create_snippet('jhat', [[\mathbf{\hat{j}}]]),
+	create_snippet('khat', [[\mathbf{\hat{k}}]]),
+	create_snippet('nll', [[\newline]]),
+	create_snippet('npp', [[\newpage]]),
+	create_snippet('tcred', [[\textcolor{red}{<>}]], { i(1) }),
+	create_snippet('img', [[![<>](<>){width=<> height=<>}]], { i(1, 'alt-text'), i(2, 'link'), i(3), i(4) }),
+	s(
+		{ trig = 'summation' },
+		fmta(
+			[[\sum_{<>}^{<>}<>]],
+			{ i(1, 'from'), i(2, 'to'), i(3, 'summand') }
+		)
+	),
+	s(
+		{ trig = 'productation' },
+		fmta(
+			[[\prod_{<>}^{<>}<>]],
+			{ i(1, 'from'), i(2, 'to'), i(3, 'productand') }
+		)
+	),
+	create_snippet('-subsection', [[\subsection{<>}]], { i(1) }),
+	s({ trig = 'italic' }, fmta([[\textit{<>}]], { i(1) })),
+	s({ trig = 'bold' }, fmta([[\textbf{<>}]], { i(1) })),
+	s({ trig = 'mono' }, fmta([[\texttt{<>}]], { i(1) })),
+	s({ trig = 'mnn', snippetType = 'autosnippet', wordTrig = false }, fmta([[\texttt{<>}]], { i(1) })),
+	s(
+		{ trig = 'itemize' },
+		fmta(
+			[[
 \begin{itemize}
   \item <>
   \item <>
   \item <>
 \end{itemize}
 ]],
-      { i(1), i(2), i(3) }
-    )
-  ),
-  s(
-    { trig = 'text' },
-    fmta(
-      [[\text{<>}]],
-      { i(1) }
-    )
-  ),
-  s(
-    { trig = 'enumerate' },
-    fmta(
-      [[
+			{ i(1), i(2), i(3) }
+		)
+	),
+	s(
+		{ trig = 'text' },
+		fmta(
+			[[\text{<>}]],
+			{ i(1) }
+		)
+	),
+	s(
+		{ trig = 'enumerate' },
+		fmta(
+			[[
 \begin{enumerate}
   \item <>
   \item <>
   \item <>
 \end{enumerate}
 ]],
-      { i(1), i(2), i(3) }
-    )
-  ),
-  s(
-    { trig = 'align' },
-    fmta(
-      [[
+			{ i(1), i(2), i(3) }
+		)
+	),
+	s(
+		{ trig = 'align' },
+		fmta(
+			[[
   \begin{align*}
   <>
   \end{align*}
 ]],
-      { i(1) }
-    )
-  ),
-  s(
-    { trig = 'begin' },
-    fmta(
-      [[
+			{ i(1) }
+		)
+	),
+	s(
+		{ trig = 'begin' },
+		fmta(
+			[[
 \begin{<>} <>
 \end{<>}
 ]],
-      { i(1), i(2), rep(1) }
-    )
-  ),
-  s(
-    { trig = 'frr', snippetType='autosnippet', wordTrig = false },
-    fmta(
-      [[\frac{<>}{<>}]],
-      { i(1), i(2) }
-    )
-  ),
-  s(
-    { trig = 'deg', snippetType='autosnippet', wordTrig = false },
-    fmta(
-      [[^\circ]],
-      {  }
-    )
-  ),
-  s(
-    { trig = 'fraction', wordTrig = false },
-    fmta(
-      [[\frac{<>}{<>}]],
-      { i(1), i(2) }
-    )
-  ),
-  s({ trig = 'suchthat', snippetType = 'autosnippet', wordTrig = false }, fmta([[.\ ]], {})),
-  s({ trig = 'suchthat', wordTrig = false }, fmta([[.\ ]], {})),
-  s({ trig = 'jk', snippetType = 'autosnippet', wordTrig = false }, fmta([[\]], {})),
-  s({ trig = 'kj', snippetType = 'autosnippet', wordTrig = false }, fmta([[\]], {})),
-  s({ trig = 'jl', snippetType = 'autosnippet', wordTrig = false }, fmta([[&]], {})),
-  s({ trig = 'lj', snippetType = 'autosnippet', wordTrig = false }, fmta([[&]], {})),
-  s({ trig = '?=', snippetType = 'autosnippet', wordTrig = false }, fmta([[\stackrel{?}{=}]], {})),
-  s({ trig = '__', snippetType = 'autosnippet', wordTrig = false }, fmta([[\_]], {})),
-  s({ trig = 'vrt', snippetType = 'autosnippet', wordTrig = false }, fmta([[&\vert &]], {})),
-  s({ trig = 'naturalnumbers' }, fmta([[\mathbb{N}]], {})),
-  s({ trig = 'integers' }, fmta([[\mathbb{Z}]], {})),
-  s({ trig = 'realnumbers' }, fmta([[\mathbb{R}]], {})),
-  s({ trig = 'rationalnumbers'}, fmta([[\mathbb{Q}]], {})),
-  s({ trig = 'vv', snippetType = 'autosnippet', wordTrig = false }, fmta([[\mathbf{<>}]], {i(1)})),
+			{ i(1), i(2), rep(1) }
+		)
+	),
+	s(
+		{ trig = 'frr', snippetType = 'autosnippet', wordTrig = false },
+		fmta(
+			[[\frac{<>}{<>}]],
+			{ i(1), i(2) }
+		)
+	),
+	s(
+		{ trig = 'deg', snippetType = 'autosnippet', wordTrig = false },
+		fmta(
+			[[^\circ]],
+			{}
+		)
+	),
+	s(
+		{ trig = 'fraction', wordTrig = false },
+		fmta(
+			[[\frac{<>}{<>}]],
+			{ i(1), i(2) }
+		)
+	),
+	s({ trig = 'suchthat', snippetType = 'autosnippet', wordTrig = false }, fmta([[.\ ]], {})),
+	s({ trig = 'suchthat', wordTrig = false }, fmta([[.\ ]], {})),
+	s({ trig = '?=', snippetType = 'autosnippet', wordTrig = false }, fmta([[\stackrel{?}{=}]], {})),
+	s({ trig = '__', snippetType = 'autosnippet', wordTrig = false }, fmta([[\_]], {})),
+	s({ trig = 'vrt', snippetType = 'autosnippet', wordTrig = false }, fmta([[&\vert &]], {})),
+	s({ trig = 'naturalnumbers' }, fmta([[\mathbb{N}]], {})),
+	s({ trig = 'integers' }, fmta([[\mathbb{Z}]], {})),
+	s({ trig = 'realnumbers' }, fmta([[\mathbb{R}]], {})),
+	s({ trig = 'rationalnumbers' }, fmta([[\mathbb{Q}]], {})),
+	s({ trig = 'vv', snippetType = 'autosnippet', wordTrig = false }, fmta([[\mathbf{<>}]], { i(1) })),
 
-  s(
-    { trig = 'displaymath' },
-    fmta(
-      '\\[\n<>\n\\]',
-      { i(1), }
-    )
-  ),
-  s(
-    { trig = 'colorblue' },
-    fmta(
-      [[
+	s(
+		{ trig = 'displaymath' },
+		fmta(
+			'\\[\n<>\n\\]',
+			{ i(1), }
+		)
+	),
+	s(
+		{ trig = 'colorblue' },
+		fmta(
+			[[
   \color{blue}
   <>
   \color{black}
   ]],
-      { i(1), }
-    )
-  ),
-  s(
-    { trig = 'deltahat' },
-    fmta(
-      [[\hat{\delta}]],
-      {  }
-    )
-  ),
-  s(
-    { trig = 'Language' },
-    fmta(
-      [[\mathcal{L}]],
-      {  }
-    )
-  ),
+			{ i(1), }
+		)
+	),
+	s(
+		{ trig = 'deltahat' },
+		fmta(
+			[[\hat{\delta}]],
+			{}
+		)
+	),
+	s(
+		{ trig = 'Language' },
+		fmta(
+			[[\mathcal{L}]],
+			{}
+		)
+	),
 }
