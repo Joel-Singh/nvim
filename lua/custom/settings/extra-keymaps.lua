2785ee7a98bb3a39ddebf95139b6d5beff0a3cfa
Joel Singh <joelsingh788@gmail.com>
2025-07-21 11:59:57 -0400

    Add key maps for moving lines

--- lua/custom/settings/extra-keymaps.lua
+++ lua/custom/settings/extra-keymaps.lua
@@ -12,3 +12,6 @@ vim.keymap.set('i', '<C-i>', '<BS>')
 
 vim.keymap.set('i', '{}<CR>', '{<CR>}<Esc>O', { desc = 'Open Braces' })
 vim.keymap.set('i', '()<CR>', '(<CR>)<Esc>O', { desc = 'Open Parentheses' })
+
+vim.keymap.set('n', '<A-j>', 'ddp', { desc = 'Move line down' })
+vim.keymap.set('n', '<A-k>', 'ddkP', { desc = 'Move line up' })
