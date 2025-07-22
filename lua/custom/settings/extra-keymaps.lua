13d16e86a7704a9b5d4f80ab5c7b2bef6fa472ae
Joel Singh <joelsingh788@gmail.com>
2025-07-22 11:52:18 -0400

    Add horizontal split keymap

--- lua/custom/settings/extra-keymaps.lua
+++ lua/custom/settings/extra-keymaps.lua
@@ -10,3 +10,5 @@ vim.keymap.set('i', '()<CR>', '(<CR>)<Esc>O', { desc = 'Open Parentheses' })
 
 vim.keymap.set('n', '<A-j>', 'ddp', { desc = 'Move line down' })
 vim.keymap.set('n', '<A-k>', 'ddkP', { desc = 'Move line up' })
+
+vim.keymap.set('n', '<C-s>', '12<C-w><C-s><Esc>:lua require("telescope.builtin").find_files()<CR>', { desc = 'Create horizontal split' })
