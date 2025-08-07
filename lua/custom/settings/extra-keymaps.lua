fb62b7c87c926535c3d767936770e5055ebcc480
Joel Singh <joelsingh788@gmail.com>
2025-08-07 14:15:40 -0400

    Add key map for calendar and today's date

--- lua/custom/settings/extra-keymaps.lua
+++ lua/custom/settings/extra-keymaps.lua
@@ -1,8 +1,6 @@
 vim.keymap.set('n', '<Leader>W', '<CMD>w<CR>', { desc = 'Write file' })
 vim.keymap.set('n', '<Leader>E', '<CMD>e<CR>', { desc = 'Edit file' })
 
-vim.keymap.set('n', '<Leader>cc', '<CMD>CodeCompanionChat<CR>', { desc = '[C]ode [C]hat' })
-
 vim.keymap.set('i', '<C-i>', '<BS>')
 
 vim.keymap.set('i', '{}<CR>', '{<CR>}<Esc>O', { desc = 'Open Braces' })
@@ -12,3 +10,5 @@ vim.keymap.set('n', '<A-j>', 'ddp', { desc = 'Move line down' })
 vim.keymap.set('n', '<A-k>', 'ddkP', { desc = 'Move line up' })
 
 vim.keymap.set('n', '<C-s>', '12<C-w><C-s><Esc>', { desc = 'Create horizontal split' })
+
+vim.keymap.set('n', '<Leader>d', '<CMD>r !cal -3v<CR>o<esc><CMD>r !date <CR>', { desc = 'Create horizontal split' })
