057507a314c80efeaaef499500afc99544504d06
Joel Singh <joelsingh788@gmail.com>
2026-04-28 19:47:48 -0400

    Move keymaps into keymap file

--- lua/custom/settings/keymaps.lua
+++ lua/custom/settings/keymaps.lua
@@ -1,3 +1,16 @@
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+vim.keymap.set('t', '<C-space>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('n', '<Leader>T', '<cmd>tabnew<CR><cmd>terminal<CR>ilazygit && e<CR>', { desc = 'Enter Lazygit With Terminal' })
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<C-c>', '<C-w><C-c>', { desc = 'Close window' })
+
 vim.keymap.set('n', '<Leader>W', '<CMD>w<CR>', { desc = 'Write file' })
 vim.keymap.set('n', '<Leader>E', '<CMD>e<CR>', { desc = 'Edit file' })
 
