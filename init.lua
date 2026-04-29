3c61052310f0ca861a9203a563ebd5346d361904
Joel Singh <joelsingh788@gmail.com>
2026-04-28 20:41:16 -0400

    Move yanking into autocmds

--- init.lua
+++ init.lua
@@ -1,17 +1,6 @@
 -- Here and not keymaps because needs to be set before lazy
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.hl.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.hl.on_yank()
-  end,
-})
 vim.g.have_nerd_font = true
 
 -- [[ Install `lazy.nvim` plugin manager ]]
--- lua/custom/settings/autocmds.lua
+++ lua/custom/settings/autocmds.lua
@@ -1,4 +1,13 @@
--- vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, { pattern = { '*.md' }, command = 'set tw=60' })
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
 
 vim.api.nvim_create_autocmd(
   { 'BufWritePost' },
