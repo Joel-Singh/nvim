2a934508aec206a8139fc2b35eaafd84cf78f05f
Joel Singh <joelsingh788@gmail.com>
2026-08-23 12:22:39 -0400

    Ignore unused locals

--- lua/custom/snippets/typst.lua
+++ lua/custom/snippets/typst.lua
@@ -1,28 +1,79 @@
+---@diagnostic disable-next-line: unused-local
 local ls = require 'luasnip'
+
+---@diagnostic disable-next-line: unused-local
 local s = ls.snippet
+
+---@diagnostic disable-next-line: unused-local
 local sn = ls.snippet_node
+
+---@diagnostic disable-next-line: unused-local
 local isn = ls.indent_snippet_node
+
+---@diagnostic disable-next-line: unused-local
 local t = ls.text_node
+
+---@diagnostic disable-next-line: unused-local
 local i = ls.insert_node
+
+---@diagnostic disable-next-line: unused-local
 local f = ls.function_node
+
+---@diagnostic disable-next-line: unused-local
 local c = ls.choice_node
+
+---@diagnostic disable-next-line: unused-local
 local d = ls.dynamic_node
+
+---@diagnostic disable-next-line: unused-local
 local r = ls.restore_node
+
+---@diagnostic disable-next-line: unused-local
 local events = require 'luasnip.util.events'
+
+---@diagnostic disable-next-line: unused-local
 local ai = require 'luasnip.nodes.absolute_indexer'
+
+---@diagnostic disable-next-line: unused-local
 local extras = require 'luasnip.extras'
+
+---@diagnostic disable-next-line: unused-local
 local l = extras.lambda
+
+---@diagnostic disable-next-line: unused-local
 local rep = extras.rep
+
+---@diagnostic disable-next-line: unused-local
 local p = extras.partial
+
+---@diagnostic disable-next-line: unused-local
 local m = extras.match
+
+---@diagnostic disable-next-line: unused-local
 local n = extras.nonempty
+
+---@diagnostic disable-next-line: unused-local
 local dl = extras.dynamic_lambda
+
+---@diagnostic disable-next-line: unused-local
 local fmt = require('luasnip.extras.fmt').fmt
+
+---@diagnostic disable-next-line: unused-local
 local fmta = require('luasnip.extras.fmt').fmta
+
+---@diagnostic disable-next-line: unused-local
 local conds = require 'luasnip.extras.expand_conditions'
+
+---@diagnostic disable-next-line: unused-local
 local postfix = require('luasnip.extras.postfix').postfix
+
+---@diagnostic disable-next-line: unused-local
 local types = require 'luasnip.util.types'
+
+---@diagnostic disable-next-line: unused-local
 local parse = require('luasnip.util.parser').parse_snippet
+
+---@diagnostic disable-next-line: unused-local
 local ms = ls.multi_snippet
 
 return {
